da9a628271030a12094c0d68137202dffcc2ee69
rashmin <rashmin9c@gmail.com>
2025-12-13 16:34:19 +0530

    ch5

--- chapter4_ownership/change_ref.rs
+++ chapter4_ownership/change_ref.rs
@@ -8,4 +8,12 @@ fn change(some_string: &mut String) {
     some_string.push_str(", world");
 }
 
-// you cannot have multiple mutable references to the same data at the same time.
+// you cannot have multiple mutable references to the same data at the same time.cd .
+
+    // let mut s = String::from("hello");
+
+    // let r1 = &s; // no problem
+    // let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+
+    // println!("{r1}, {r2}, and {r3}");
---
+++ chapter5_struct/structs/src/main.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    print!("{:#?}", rect1);
+    println!(
+        "\nThe area of the rectangleis {} square pixels.", rect1.area()
+    );
+}
