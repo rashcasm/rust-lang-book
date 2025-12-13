2f53b00698f439f64b5e66d118854d8424baa6c3
rashmin <rashmin9c@gmail.com>
2025-12-13 12:06:13 +0530

    chapter 4 remaining

---
+++ chapter4_ownership/change_ref.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+// you cannot have multiple mutable references to the same data at the same time.
---
+++ chapter4_ownership/ref.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let s = String::from("hello");
+    let len = calculate_length(&s);
+    println!("The length of '{}' is {}.", s, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+    // references are immutable by default
+}
