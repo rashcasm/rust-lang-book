c02521873101b50a975adfb622a1b90125fcadf0
rashmin <rashmin9c@gmail.com>
2025-12-13 11:23:20 +0530

    chapter 1

---
+++ chapter1/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18.d
@@ -0,0 +1,5 @@
+/home/rashmin/Codeshit/rust-lang-book/chapter1/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18.d: src/main.rs
+
+/home/rashmin/Codeshit/rust-lang-book/chapter1/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18: src/main.rs
+
+src/main.rs:
---
+++ chapter1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/rashmin/Codeshit/rust-lang-book/chapter1/hello_cargo/target/debug/hello_cargo: /home/rashmin/Codeshit/rust-lang-book/chapter1/hello_cargo/src/main.rs
