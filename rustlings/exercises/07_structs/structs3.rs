8a34f1b6285608b924e586a8b54b448a351e38ba
rashmin <rashmin9c@gmail.com>
2025-12-14 15:51:59 +0530

    rustlings ch8

--- rustlings/exercises/07_structs/structs3.rs
+++ rustlings/exercises/07_structs/structs3.rs
@@ -15,7 +15,6 @@ impl Package {
             // learn about error handling later.
             panic!("Can't ship a package with weight below 10 grams");
         }
-
         Self {
             sender_country,
             recipient_country,
@@ -24,14 +23,16 @@ impl Package {
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn is_international(&self) {
+    fn is_international(&self) -> bool{
         // TODO: Read the tests that use this method to find out when a package
         // is considered international.
+        self.sender_country != self.recipient_country
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn get_fees(&self, cents_per_gram: u32) {
+    fn get_fees(&self, cents_per_gram: u32) -> u32{
         // TODO: Calculate the package's fees.
+        self.weight_in_grams * cents_per_gram
     }
 }
 
