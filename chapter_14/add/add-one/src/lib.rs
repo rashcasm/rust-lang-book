be0f4eff0e955931f053d0acfe61b57277eb736c
rashmin <rashmin9c@gmail.com>
2025-12-26 01:43:08 +0530

    workspace ch14

---
+++ chapter_14/add/add-one/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn add_one(x: i64) -> i64 {
+    x+1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(3, add_one(2));
+    }
+}
---
+++ chapter_14/add/adder/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let x = 5;
+    println!("{} + 1 = {}", x, add_one::add_one(x));
+}
