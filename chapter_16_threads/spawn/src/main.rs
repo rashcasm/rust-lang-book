6f1360363dcbd3f72ce9a6abd32ac6a0744085ff
rashmin <rashmin9c@gmail.com>
2025-12-30 20:34:18 +0530

    threads

--- chapter_16_threads/spawn/src/main.rs
+++ chapter_16_threads/spawn/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
+            thread::sleep(Duration::from_millis(500));
         }
     });
 
     for i in 1..5 {
         println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
+        thread::sleep(Duration::from_millis(1000));
     }
 }
